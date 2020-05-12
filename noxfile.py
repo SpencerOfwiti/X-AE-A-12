4f2a1c63a7c20dbd6c39a525c1f53946c3979145
SpencerOfwiti <maxspencer56@gmail.com>
2020-05-12 16:44:47 +0300

    Add nox sessions. - Linting with flake8 and black. - Check for the safety of dependencies

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,81 @@
+import tempfile
+
 import nox
+
+locations = "src", "tests", "noxfile.py"
+nox.options.sessions = "lint", "safety", "tests"
+
+
+def install_with_constraints(session, *args, **kwargs):
+    """
+    Install packages constrained by Poetry's lock file.
+    :param session: The sessions object.
+    :param args: Command-line arguments for pip.
+    :param kwargs: Additional keyword arguments for Session.install.
+    :return:
+    """
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=["3.8", "3.7"])
 def test(session):
-	"""
+    """
 	Run the test suite.
 	:param session:
 	:return:
 	"""
-	args = session.posargs or ["--cov", "-m", "not e2e"]
-	session.run("poetry", "install", external=True)
-	session.run("pytest", *args)
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
+    session.run("pytest", *args)
+
+
+@nox.session(python=["3.8", "3.7"])
+def lint(session):
+    """Lint using flake8"""
+    args = session.posargs or locations
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.8", "3.7"])
+def black(session):
+    """Run black code formatter."""
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
+
+
+@nox.session(python="3.8")
+def safety(session):
+    """Scan dependencies for insecure packages."""
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        install_with_constraints(session, "safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
