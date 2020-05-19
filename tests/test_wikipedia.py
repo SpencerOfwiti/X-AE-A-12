f4cf176238ce1e0910a6c83f545aadb0eee96981
SpencerOfwiti <maxspencer56@gmail.com>
2020-05-19 19:21:38 +0300

    Add tests for page datatype and typeguard. - Add type annotation.

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,7 +1,11 @@
+from unittest.mock import Mock
+
+import click
+import pytest
 from src.x_ae_a_12 import wikipedia
 
 
-def test_random_page_uses_given_language(mock_requests_get):
+def test_random_page_uses_given_language(mock_requests_get: Mock) -> None:
     """
 	It selects the specified Wikipedia language edition.
 	:param mock_requests_get:
@@ -10,3 +14,21 @@ def test_random_page_uses_given_language(mock_requests_get):
     wikipedia.random_page(language="de")
     args, _ = mock_requests_get.call_args
     assert "de.wikipedia.org" in args[0]
+
+
+def test_random_page_returns_page(mock_requests_get: Mock) -> None:
+    page = wikipedia.random_page()
+    assert isinstance(page, wikipedia.Page)
+
+
+def test_random_page_handles_validation_errors(mock_requests_get: Mock) -> None:
+    mock_requests_get.return_value.__enter__.return_value.json.return_value = None
+    with pytest.raises(click.ClickException):
+        wikipedia.random_page()
+
+
+def test_trigger_typeguard(mock_requests_get: Mock) -> None:
+    import json
+
+    data = json.loads('{"language": 1}')
+    wikipedia.random_page(language=data["language"])
