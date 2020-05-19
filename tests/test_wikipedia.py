a40f8a3698cdf924b408f74b89ceaa57a39fe019
SpencerOfwiti <maxspencer56@gmail.com>
2020-05-19 19:26:47 +0300

    Add documentation for test functions.

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -17,17 +17,20 @@ def test_random_page_uses_given_language(mock_requests_get: Mock) -> None:
 
 
 def test_random_page_returns_page(mock_requests_get: Mock) -> None:
+    """Tests if a page datatype is returned."""
     page = wikipedia.random_page()
     assert isinstance(page, wikipedia.Page)
 
 
 def test_random_page_handles_validation_errors(mock_requests_get: Mock) -> None:
+    """Tests if validation errors are handled."""
     mock_requests_get.return_value.__enter__.return_value.json.return_value = None
     with pytest.raises(click.ClickException):
         wikipedia.random_page()
 
 
 def test_trigger_typeguard(mock_requests_get: Mock) -> None:
+    """Tests if typeguard has been triggered."""
     import json
 
     data = json.loads('{"language": 1}')
