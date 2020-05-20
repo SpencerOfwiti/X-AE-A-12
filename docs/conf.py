2963b11122e659722dc83254e8f4772c4be42ba5
SpencerOfwiti <maxspencer56@gmail.com>
2020-05-20 17:24:20 +0300

    Add sphinx configuration file.

---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+"""Sphinx configuration."""
+project = "x-ae-a-12"
+author = "Spencer Ofwiti"
+copyright = f"2020, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
