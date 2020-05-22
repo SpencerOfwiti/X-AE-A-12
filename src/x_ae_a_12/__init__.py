f223eda73446dfd19ed6a40b88a8e6846a4acbb8
SpencerOfwiti <maxspencer56@gmail.com>
2020-05-22 11:22:31 +0300

    Refactor to single source the package version.

--- src/x_ae_a_12/__init__.py
+++ src/x_ae_a_12/__init__.py
@@ -1,2 +1,10 @@
 """The X-AE-A-12 Python project."""
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
